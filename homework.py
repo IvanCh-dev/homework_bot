9eef71cda5da4472e132e9c431029da61392a964
Ivan Chuzhba <ivan94-20@yandex.ru>
2022-10-14 12:23:05 +0600

    Fixed_2

--- homework.py
+++ homework.py
@@ -4,6 +4,7 @@ import time
 import telegram
 import logging
 from dotenv import load_dotenv
+from http import HTTPStatus
 
 
 load_dotenv()
@@ -30,6 +31,7 @@ HOMEWORK_STATUSES = {
 
 def send_message(bot, message):
     """Отправка сообщения ботом."""
+    logging.info('Попытка бота отправить сообщение в чат')
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logging.info('Бот отправил сообщение в чат')
@@ -40,45 +42,42 @@ def send_message(bot, message):
 def get_api_answer(current_timestamp=int(time.time())):
     """Получения ответа от API Практикума."""
     params = {'from_date': current_timestamp}
-    homework_statuses = requests.get(
-        ENDPOINT, headers=HEADERS, params=params)
-    if homework_statuses.status_code != 200:
-        logging.error('Недоступность эндпоинта API Практикума')
-        raise Exception('Недоступность эндпоинта API Практикума')
-    return homework_statuses.json()
+    logging.info('Попытка получить ответ от API Практикума')
+    try:
+        homework_statuses = requests.get(
+            ENDPOINT, headers=HEADERS, params=params)
+        if homework_statuses.status_code != HTTPStatus.OK:
+            logging.error('Недоступность эндпоинта API Практикума')
+            raise Exception('Недоступность эндпоинта API Практикума')
+        return homework_statuses.json()
+    except Exception as error:
+        logging.error(f'Ошибка получить ответ от API Практикума: {error}')
+        raise Exception(f'Ошибка получить ответ от API Практикума: {error}')
 
 
 def check_response(response):
     """Проверка ответа от API Практикума."""
-    if type(response) == dict:
-        if len(response) == 0:
-            logging.error('API вернул пустой словарь')
-            raise Exception('API вернул пустой словарь')
-        if 'homeworks' not in response.keys():
-            logging.error('В ответе API отсутствует ключ "homeworks"')
-            raise Exception('В ответе API отсутствует ключ "homeworks"')
-        if 'current_date' not in response.keys():
-            logging.error('В ответе API отсутствует ключ "current_date"')
-            raise Exception('В ответе API отсутствует ключ "current_date"')
-        if type(response['homeworks']) != list:
-            logging.error('Значение ключа "homeworks" не список')
-            raise Exception('Значение ключа "homeworks" не список')
-        return response.get('homeworks')
-    return response
+    if not isinstance(response, dict):
+        raise TypeError('Ответ API не является словарём')
+    homeworks = response.get('homeworks')
+    if 'homeworks' not in response.keys():
+        raise Exception('В ответе API отсутствует ключ "homeworks"')
+    if 'current_date' not in response.keys():
+        raise Exception('В ответе API отсутствует ключ "current_date"')
+    if not isinstance(homeworks, list):
+        raise Exception('Значение ключа "homeworks" не список')
+    return homeworks
 
 
 def parse_status(homework):
     """Получение статуса домашней работы."""
     if 'homework_name' not in homework:
-        logging.error('Ключ homework_name отсутсвует в ответе API Практикума')
         raise KeyError('Ключ homework_name отсутсвует в ответе API Практикума')
     if 'status' not in homework:
-        logging.error('Ключ status отсутвует в ответе API Практикума')
         raise KeyError('Ключ status отсутвует в ответе API Практикума')
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
     if homework_status not in HOMEWORK_STATUSES:
-        logging.error(f'Неизвестный статус {homework_status}')
         raise Exception(f'Неизвестный статус {homework_status}')
     verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -86,41 +85,31 @@ def parse_status(homework):
 
 def check_tokens():
     """Проверка наличия токенов."""
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-        return True
-    logging.critical('Отсутствие обязательных переменных окружения')
-    return False
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 
 def main():
     """Основная логика работы бота."""
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = 0
-    homeworks_dct = {}
-    prev_response = None
-    while True:
-        try:
-            response = get_api_answer(current_timestamp=current_timestamp)
-            if response != prev_response:
-                for homework in response.get('homeworks'):
-                    homework_name = homework.get('homework_name')
-                    homework_status = homework.get('status')
-                    if homework_name in homeworks_dct:
-                        if homework_status != homeworks_dct.get(homework_name):
-                            homeworks_dct[homework_name] = homework_status
-                            status = parse_status(homework)
-                            send_message(bot, status)
-                    else:
-                        homeworks_dct[homework_name] = homework_status
-                        status = parse_status(homework)
-                        send_message(bot, status)
-            prev_response = response
-            current_timestamp = int(time.time())
-            time.sleep(RETRY_TIME)
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            logging.error(message)
-            time.sleep(RETRY_TIME)
+    if check_tokens():
+        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        current_timestamp = 0
+        while True:
+            try:
+                response = get_api_answer(current_timestamp)
+                homeworks = check_response(response)
+                if homeworks:
+                    status = parse_status(homeworks[0])
+                    send_message(bot, status)
+                else:
+                    logging.info('Изменения в домашних работах отсутствуют')
+            except Exception as error:
+                logging.error(f'Ошибка: {error}')
+            finally:
+                current_timestamp = int(time.time())
+                time.sleep(RETRY_TIME)
+    else:
+        logging.critical('Отсутствие обязательных переменных окружения')
+        raise Exception('Отсутствие обязательных переменных окружения')
 
 
 if __name__ == '__main__':
